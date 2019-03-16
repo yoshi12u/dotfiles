d15cceb53cbaa608e2e815c233d918c199266f27
uesaiso <37270772+uesaiso@users.noreply.github.com>
2019-03-16 16:15:45 +0900

    Update path.zsh

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -9,4 +9,15 @@ fi
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
+# anyenv
+if [ -d $HOME/.anyenv ] ; then
+    export PATH="$HOME/.anyenv/bin:$PATH"
+    eval "$(anyenv init -)"
+    # for tmux
+    for D in `\ls $HOME/.anyenv/envs`
+    do
+        export PATH="$HOME/.anyenv/envs/$D/shims:$PATH"
+    done
+fi
+
 export -U PATH
