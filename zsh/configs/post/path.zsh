e432c365bd5e232244be084eae9d4e98c77e2816
uesaiso <yoshimasa.u@c-seikyo104noMacBook-Air.local>
2019-03-18 23:34:11 +0900

    edit README.md

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -9,16 +9,4 @@ fi
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
-# anyenv
-if [ ! -d ${HOME}/.anyenv ] ; then
-  eval "git clone https://github.com/anyenv/anyenv ~/.anyenv"
-eval "$(anyenv install  --init)"
-fi
-export PATH="$HOME/.anyenv/bin:$PATH"
-eval "$(anyenv init -)"
-for D in `ls $HOME/.anyenv/envs`
-do
-  export PATH="$HOME/.anyenv/envs/$D/shims:$PATH"
-done
-
 export -U PATH
