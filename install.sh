b4828f6857edf66eb6bb8ad0f035130b2031df1f
uesaiso <yoshimasa.u@c-seikyo104noMacBook-Air.local>
2019-03-26 00:16:09 +0900

    exclusively for mac

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
-if [ "$(uname)" == 'Darwin' ]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-  test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-  test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-fi
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 mkdir ~/dotfiles
 git clone https://github.com/uesaiso/dotfiles ~/dotfiles
 cd ~/dotfiles
