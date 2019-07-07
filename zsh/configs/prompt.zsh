f4c4ba17a4bc14d5efa2722d9535e7f712f9e720
uesaiso <u7ubtaj4@gmail.com>
2019-07-07 13:25:13 +0900

    delete toggl and todoist

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,24 +1,7 @@
-prompt_toggl_current() {
-  local tgc=$(toggl --cache --csv current)
-  local tgc_time=$(echo $tgc | grep Duration | cut -d ',' -f 2)
-  local tgc_dsc=$(echo $tgc | grep Description | cut -d ',' -f 2 | cut -c 1-20)
-  local short_tgc_dsc=$(if [ $(echo $tgc_dsc | wc -m) -lt 20 ]; then echo $tgc_dsc; else echo "${tgc_dsc}.."; fi)
-  if [ ! -n "$tgc_time" ]; then
-      echo "None"
-  else
-      echo "[$tgc_time $short_tgc_dsc]"
-  fi
-}
-
 preprompt_custom() {
   echo ''
 }
 
-preprompt_newline() {
-  echo '%F{white}Task: $(prompt_toggl_current)%f'
-}
-
 prompt_newline='%666v'
 PROMPT=" $(preprompt_custom)
-$(preprompt_newline)
 $PROMPT"
