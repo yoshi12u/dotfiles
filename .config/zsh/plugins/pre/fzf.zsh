e557c582d4553b187b2844f9880c22de33b1502e
uesaiso <ysmsuehr@gmail.com>
2022-05-18 21:42:26 +0900

    change fzf color scheme

--- .config/zsh/plugins/pre/fzf.zsh
+++ .config/zsh/plugins/pre/fzf.zsh
@@ -2,8 +2,9 @@
 
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!{.git,node_modules,tmp}/*"'
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
---color=dark
---color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe
---color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef
+--color=fg:#cbccc6,bg:#1f2430,hl:#707a8c
+--color=fg+:#707a8c,bg+:#191e2a,hl+:#ffcc66
+--color=info:#73d0ff,prompt:#707a8c,pointer:#cbccc6
+--color=marker:#73d0ff,spinner:#73d0ff,header:#d4bfff
 --height 40% --reverse 
 '
