e7fbd113fa2cd6f943d2fb0e5ec9988373794c4f
uesaiso <ysmsuehr@gmail.com>
2023-08-19 20:57:51 +0900

    update

--- .config/nvim/lua/plugins/operator.lua
+++ .config/nvim/lua/plugins/operator.lua
@@ -2,8 +2,26 @@ return {
   {
     "gbprod/substitute.nvim",
     keys = {
-      { "R", "<cmd>lua require('substitute').operator()<cr>", silent = true },
+      {
+        "R",
+        function()
+          require("substitute").operator()
+        end,
+        remap = false,
+        desc = "Substitute operator",
+      },
+      {
+        "R",
+        mode = "x",
+        function()
+          require("substitute").visual()
+        end,
+        remap = false,
+        desc = "Substitute visual",
+      },
     },
-    lazy = false,
+    opts = {
+      on_substitute = require("yanky.integration").substitute(),
+    },
   },
 }
--- .config/nvim/lua/plugins/surronud.lua
+++ .config/nvim/lua/plugins/surronud.lua
@@ -1,11 +1,22 @@
 return {
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-  },
+  -- {
+  --   "kylechui/nvim-surround",
+  --   version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  --   event = "VeryLazy",
+  -- },
   {
     "echasnovski/mini.surround",
-    enabled = false,
+    enable = false,
+    opts = {
+      mappings = {
+        add = "ys",
+        delete = "ds",
+        -- find = "<",
+        -- find_left = "gsF",
+        -- highlight = "gsh",
+        replace = "cs",
+        -- update_n_lines = "gsn",
+      },
+    },
   },
 }
