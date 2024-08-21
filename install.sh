2e955a74ce8accd4db25a6daf22987bce4eb1d25
yoshi12u <ysmsuehr@gmail.com>
2024-08-22 08:46:36 +0900

    revert

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --no-confirm --init none
 nix run home-manager/release-24.05 -- switch --flake .#x86_64-devcontainer
