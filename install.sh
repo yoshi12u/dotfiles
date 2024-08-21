46b8c7661b882d48173e4be87813c5eff2655584
yoshi12u <ysmsuehr@gmail.com>
2024-08-22 08:43:13 +0900

    move nix to devcontainer feature realm

--- install.sh
+++ install.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# For devcontainer
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --no-confirm --init none
 nix run home-manager/release-24.05 -- switch --flake .#x86_64-devcontainer
