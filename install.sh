c8de1cce6096cf94bad432a39d1c6437dbbbab20
yoshi12u <ysmsuehr@gmail.com>
2024-08-22 09:09:18 +0900

    update devcontainer

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --no-confirm --init none
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 nix run home-manager/release-24.05 -- switch --flake .#x86_64-devcontainer
