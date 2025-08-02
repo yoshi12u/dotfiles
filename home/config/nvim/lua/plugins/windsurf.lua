536f927e2463eb4f2ad3a10802f501e295a3f9e6
yoshi12u <ysmsuehr@gmail.com>
2025-08-02 20:44:30 +0900

    feat: remove unused nvim plugins and dependencies

--- home/config/nvim/lua/plugins/windsurf.lua
+++ home/config/nvim/lua/plugins/windsurf.lua
@@ -3,7 +3,6 @@ return {
     "Exafunction/windsurf.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
     },
     config = function()
       require("codeium").setup({})
