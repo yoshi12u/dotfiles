b885ee3328ee239b32951770d9bf25e5b52ee65c
yoshi12u <ysmsuehr@gmail.com>
2025-08-31 15:03:42 +0900

    refactor: simplify Karabiner config and update editor settings

--- home/config/nvim/lua/config/lazy.lua
+++ home/config/nvim/lua/config/lazy.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.editor.inc-rename" },
 		{ import = "lazyvim.plugins.extras.editor.refactoring" },
 		{ import = "lazyvim.plugins.extras.editor.mini-diff" },
+		{ import = "lazyvim.plugins.extras.editor.snacks_explorer" },
 		{ import = "lazyvim.plugins.extras.formatting.biome" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
