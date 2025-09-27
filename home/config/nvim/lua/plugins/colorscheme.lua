94d01afc4def7cac43b388b6b353e94d03dbee68
yoshi12u <ysmsuehr@gmail.com>
2025-09-27 12:46:07 +0900

    chore: clean up Karabiner and colorscheme tweaks

--- home/config/nvim/lua/plugins/colorscheme.lua
+++ home/config/nvim/lua/plugins/colorscheme.lua
@@ -10,16 +10,6 @@ return {
 		},
 	},
 	{
-		"catppuccin/nvim",
-		opts = function(_, opts)
-			local module = require("catppuccin.groups.integrations.bufferline")
-			if module then
-				module.get = module.get_theme
-			end
-			return opts
-		end,
-	},
-	{
 		"LazyVim/LazyVim",
 		opts = {
 			colorscheme = "catppuccin",
